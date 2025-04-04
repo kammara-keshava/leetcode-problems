859242aee38a8a9bfe71a73349dda63afadb87b6
kammara-keshava <151363158+kammara-keshava@users.noreply.github.com>
2025-04-04 12:09:24 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,30 @@
+//ParseInt Method of Integer class
+public class Strings {
+   public static void main(String args[]) {
+       String str = "123";
+       int number = Integer.parseInt(str);
+       System.out.println(number);
+      
+      
+   }
+}
+
+
+//reverse string
+// public class Strings{
+//     public static void main(String[] args){
+//         String s = "kesh";
+//         StringBuilder reversedString = new StringBuilder(s);
+//      for(int i=0;i<s.length()/2;i++){
+//         int front=i;
+//         int back=s.length()-1-i;
+//         char frontchar=s.charAt(front);
+//         char backchar=s.charAt(back);
+//         reversedString.setCharAt(front, backchar);
+//             reversedString.setCharAt(back, frontchar);
+
+
+//      }
+//      System.out.println(reversedString);
+//     }
+// }
